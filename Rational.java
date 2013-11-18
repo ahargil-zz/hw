8e2bec88f5ce8cc0134cfffbae7b36170dfd4dea
Anya Hargil <anya.hargil@gmail.com>
2013-11-18 18:00:12 -0500

    Create Rational.java

---
+++ Rational.java
@@ -0,0 +1,56 @@
+public class Rational{
+
+  /*======================================================================================
+  Instance variables for the numerator and denominator
+  ======================================================================================*/
+  private int _num, _den;
+  
+  /*======================================================================================
+  Default constructor (no parameters)
+  Creates a new Rational with the value of 0/1
+  ======================================================================================*/
+  
+  public Rational() {
+    Rational(0, 1);
+  }
+  
+  /*======================================================================================
+  Constructor
+  takes 2 parameters, one for the numerator, one for the denominator
+  if an invalid denominator is attempted, should print a message and set the number to 0/1
+  ======================================================================================*/
+  
+  public Rational(num, den) {
+    if (den == 0) {
+      System.out.println("Zero is an invalid denomenator");
+      _num = 0;
+      _den = 1;
+    }
+    else {
+      _num = num;
+      _den = den;
+    }
+  }
+  
+  /*=======================================================================================
+  toString
+  returns a string representation of the rational number (formatting of your choice)
+  =======================================================================================*/
+  
+  public String toString() {
+    String ret = "";
+    ret += _num;
+    ret += "/";
+    ret += _den;
+  }
+  
+  public static void main( String[] args ) {
+    Rational q = new Rational();
+    Rational r = new Rational(2,3); //Stores the rational number 2/3
+    Rational s = new Rational(1,2); //Stores the rational number 1/2
+    System.out.println("The rational number q is " + q);
+    System.out.println("The rational number r is " + r);
+    System.out.println("The rational number s is " + s);
+  }
+}
+    
